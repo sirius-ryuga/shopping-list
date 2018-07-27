340f2e3a392944132bf4d1725a9f59389b492b77
Maria Ivanchenko <draco_aeternitas@icloud.com>
2018-07-27 14:21:40 +0500

    shopping list

---
+++ array.py
@@ -0,0 +1,13 @@
+array = []
+
+print("Список покупок")
+print("Нужно купить: Ничего")
+while True:
+    a = (str(input("Что добавить?" + "\n")))
+    array.append(a)
+    print("Нужно купить: {}".format(array))
+
+
+
+
+
